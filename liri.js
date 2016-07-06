17a26c3495447ecf9cb3d8334c35bfc5c7fcacb3
adam fader <adamfader@gmail.com>
2016-07-06 14:37:42 -0400

    added spotify api

--- liri.js
+++ liri.js
@@ -2,15 +2,18 @@ var request = require('request');
 var Twitter = require('twitter');
 var keys = require('./keys.js');
 var twit = new Twitter(keys.twitterKeys);
-var userInput = process.argv[2];
+var spotify = require('spotify');
+var userInput1 = process.argv[2];
+var userInput2 = process.argv[3];
 
-if(userInput == "my-tweets"){
+if(userInput1 == 'my-tweets'){
   tweeter();
+}else if(userInput1 == 'spotify-this-song'){
+  spotty();
 }
 
 function tweeter(){
 
-  //var params = {screen_name: 'adamfader'};
   twit.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=adamfader&count=20', function(error, tweets, response){
     if(!error){
       for(i = 0; i < tweets.length; i++){
@@ -20,17 +23,20 @@ function tweeter(){
       console.log(error);
     }
   });
-  /**
- * Stream statuses filtered by keyword
- * number of tweets per second depends on topic popularity
- **/
-  // client.stream('statuses/filter', {track: 'adamfader'},  function(stream) {
-  //   stream.on('data', function(tweet) {
-  //     console.log(tweet.text);
-  //   });
 
-  //   stream.on('error', function(error) {
-  //     console.log(error);
-  //   });
-  // });
+}
+
+function spotty(){
+
+  spotify.search({ type: 'track', query: userInput2 }, function(err, data) {
+    if (err){
+      console.log('Error occurred: ' + err);
+      return;
+    }
+    console.log(data.tracks.items[0].artists[0].name);
+    console.log(data.tracks.items[0].name);
+    console.log(data.tracks.items[0].album.name);
+    console.log(data.tracks.items[0].preview_url); 
+  });
+
 }
