56e0ca0274c910633823dea355a10446ad8d3580
adam fader <adamfader@gmail.com>
2016-07-06 13:32:39 -0400

    fixed externalized keys

--- liri.js
+++ liri.js
@@ -1,14 +1,7 @@
 var request = require('request');
 var Twitter = require('twitter');
-//var keys = require('./keys.js');
-
-var client = new Twitter({
-  consumer_key: '9nC0QzSjyQPzNLBgvuk8Sw3UG',
-  consumer_secret: '5aiI6ByDplSrzpr4d9L3J7zb9WVtGjTfEJyi4LMk2yJphaeoeD',
-  access_token_key: '2765922531-d8ojfBl1aqvufYKZSPR95TV3ivzKm0fShxA7Hgv',
-  access_token_secret: 'SFlA6b85t5IlAl6GHpwJatmrvGjjv7fGMcNqsWuH5sMzh'
-});
-
+var keys = require('./keys.js');
+var twit = new Twitter(keys.twitterKeys);
 var userInput = process.argv[2];
 
 if(userInput == "my-tweets"){
@@ -18,9 +11,11 @@ if(userInput == "my-tweets"){
 function tweeter(){
 
   //var params = {screen_name: 'adamfader'};
-  client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=adamfader&count=3', function(error, tweets, response){
-    if (!error) {
-      console.log(tweets);
+  twit.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=adamfader&count=20', function(error, tweets, response){
+    if(!error){
+      for(i = 0; i < tweets.length; i++){
+        console.log(tweets[i].user.screen_name + ' said: ' + tweets[i].text);
+      }
     }else{
       console.log(error);
     }
