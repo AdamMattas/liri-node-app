7425e27976fa5629f58cf321728819a4308586fb
adam fader <adamfader@gmail.com>
2016-08-04 11:45:19 -0400

    removed keys.js from git ignore file

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '9nC0QzSjyQPzNLBgvuk8Sw3UG',
+  consumer_secret: '5aiI6ByDplSrzpr4d9L3J7zb9WVtGjTfEJyi4LMk2yJphaeoeD',
+  access_token_key: '2765922531-d8ojfBl1aqvufYKZSPR95TV3ivzKm0fShxA7Hgv',
+  access_token_secret: 'SFlA6b85t5IlAl6GHpwJatmrvGjjv7fGMcNqsWuH5sMzh'
+};
